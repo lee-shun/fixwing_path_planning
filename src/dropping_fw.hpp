a34516607b860d9936d6c645356b73ea05bf26d7
孙浩 <51771292+HAOSUNHH@users.noreply.github.com>
2020-04-28 15:33:02 +0800

    Update dropping_fw.hpp

--- src/dropping_fw.hpp
+++ src/dropping_fw.hpp
@@ -44,11 +44,7 @@
 #include "lib/mathlib.hpp"
 
 using namespace std;
-mavros_msgs::State current_state;
-mavros_msgs::WaypointList current_waypoints;
-mavros_msgs::WaypointReached reached_waypoints;
-sensor_msgs::NavSatFix current_gps;
-mavros_msgs::WaypointList waypoint_list;
+
 class DROPPING_FW {
 
 public:
@@ -69,6 +65,11 @@ private:
   ros::ServiceClient waypoint_pull_client;
   ros::ServiceClient waypoint_push_client;
   ros::ServiceClient waypoint_clear_client;
+  mavros_msgs::State current_state;
+  mavros_msgs::WaypointList current_waypoints;
+  mavros_msgs::WaypointReached reached_waypoints;
+  sensor_msgs::NavSatFix current_gps;
+  mavros_msgs::WaypointList waypoint_list;
   void ros_sub_pub();
 
   bool reached_waypoint(const struct _s_waypoint &waypoint);
@@ -77,5 +78,69 @@ private:
 
   mavros_msgs::Waypoint waypoint[100];/* 即将要发给px4的航点队列 */
   void plan_waypoint(int task_stage);
+  
+  const double eps = 1e-9;
+  int r = 30; //盘旋半径
+  double wc;
+  double jc;
+  double jing0, wei0;
+  double xc, yc, goal_x, goal_y;
+  double x, y;
+  class Point;
+  typedef Point Vec;
+  const double g = 9.80665;  //重力加速度
+  const float pi = 3.141593;
+  
+  struct Point {
+	double x, y;
+	Point(double _x = 0, double _y = 0) :x(_x), y(_y) {}
+	//向量与常数 注意常数要放在后面
+	Vec operator*(double p) { return Vec(x*p, y*p); }
+	Vec operator/(double p) { return Vec(x / p, y / p); }
+	Vec operator-(Vec obj) { return Vec(x - obj.x, y - obj.y); }
+	Vec operator+(Vec obj) { return Vec(x + obj.x, y + obj.y); }
+	//点积
+	double operator*(Vec obj) { return x*obj.x + y*obj.y; }
+	//叉积
+	double operator^(Vec obj) { return x*obj.y - y*obj.x; }
+	//两个向量的夹角 A*B=|A|*|B|*cos(th)
+	double Angle(Vec B) { return acos((*this)*B / (*this).len() / B.len()); }
+	//两条向量平行四边形的面积
+	double Area(Vec B) { return fabs((*this) ^ B); }
+	//向量旋转
+	//旋转公式
+	//  Nx     (cos  -sin) x
+	//  Ny     (sin   cos) y
+	Vec Rotate(double rad) { return Vec(x*cos(rad) - y*sin(rad), x*sin(rad) + y*cos(rad)); }
+	//返回向量的法向量，即旋转pi/2
+	Vec Normal() { return Vec(-y, x); }
+	//返回向量的长度,或者点距离原点的距离
+	double len() { return hypot(x, y); }
+	double len2() { return x*x + y*y; }
+	//返回两点之间的距离
+	double dis(Point obj) { return hypot(x - obj.x, y - obj.y); } //hypot 给定直角三角形的两条直角边，返回斜边边长
+	//向量的极角 atan2(y,x)
+	bool operator==(Point obj) { return dcmp(x - obj.x) == 0 && dcmp(y - obj.y) == 0; }
+	bool operator<(Point obj) { return x<obj.x || (x == obj.x&&y<obj.y); }
+};
+Point va[10], vb[10];
+
+
+
+struct Line {
+	Point A, B;
+	double l;
+	bool operator<(const Line& b)const {
+		if (dcmp(A.x - b.A.x) == 0)return A.y<b.A.y;
+		return A.x<b.A.x;
+	}
+}L[10];
+
+
+struct Circle {
+	Point c; double r;
+	Circle(Point c, double r) :c(c), r(r) {}
+	Point getpoint(double a) { return Point(c.x + cos(a)*r, c.y + sin(a)*r); }
+};
 };
 #endif
