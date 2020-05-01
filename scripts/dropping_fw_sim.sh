eabae1c7dbb1103cae38d89c2508d3ab01d1ce58
lee-shun <lee970802@163.com>
2020-05-01 16:13:50 +0800

    update thesies formation

--- scripts/dropping_fw_sim.sh
+++ scripts/dropping_fw_sim.sh
@@ -3,5 +3,5 @@
 #zsh
 gnome-terminal --window  -e 'zsh -c "cd ~/catkin_ws; catkin_make; exec zsh"' \
 --tab -e 'zsh -c "sleep 3; source ~/.zshrc&&roslaunch px4 mavros_posix_sitl.launch;  exec zsh"' \
---tab -e 'zsh -c "sleep 4; rosrun fixwing_path_planning dropping_fw; exec zsh"' \
+--tab -e 'zsh -c "sleep 8; rosrun fixwing_path_planning dropping_fw; exec zsh"' \
 
